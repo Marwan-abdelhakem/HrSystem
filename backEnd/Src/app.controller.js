e0e0ce76e5cc2b3dbe41be0e1e11a8f9a0d6f126
Marwan abdelhakem <marwanabdelhakem@gmail.com>
2026-04-25 22:42:10 +0300

    update

--- backEnd/Src/app.controller.js
+++ backEnd/Src/app.controller.js
@@ -16,7 +16,7 @@ import path from "path"
 const bootStrap = async (app, express) => {
 
     app.use(cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        origin: 'https://hr-system-aarc.vercel.app' || "http://localhost:5173",
         credentials: true,
     }))
 
